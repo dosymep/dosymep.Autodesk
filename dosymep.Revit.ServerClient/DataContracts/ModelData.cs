87531c9b49d7d546e5b7ddedad836d7ba0746e05
dosymep <dosymep@gmail.com>
2022-08-03 19:02:13 +0300

    added object data

--- dosymep.Revit.ServerClient/DataContracts/ModelData.cs
+++ dosymep.Revit.ServerClient/DataContracts/ModelData.cs
@@ -4,12 +4,7 @@ namespace dosymep.Revit.ServerClient.DataContracts {
     /// <summary>
     /// The model data.
     /// </summary>
-    public class ModelData {
-        /// <summary>
-        /// The name of folder/model.
-        /// </summary>
-        public string Name { set; get; }
-
+    public class ModelData : ObjectData {
         /// <summary>
         /// The size in bytes of the model.
         /// </summary>
@@ -24,22 +19,5 @@ namespace dosymep.Revit.ServerClient.DataContracts {
         /// The version number of Revit that the model is lasted modified with.
         /// </summary>
         public ProductVersion ProductVersion { set; get; }
-        
-        /// <summary>
-        /// The lock state of the folder/model.
-        /// </summary>
-        public LockState LockState { set; get; }
-        
-        /// <summary>
-        /// The context of the admin lock on the folder/model,
-        /// describing the use of the admin lock such as copying or moving a folder
-        /// from one server to another.
-        /// </summary>
-        public LockContext LockContext { set; get; }
-
-        /// <summary>
-        /// The list of descendant models that are locked by the Revit clients.
-        /// </summary>
-        public List<ModelLockData> ModelLocksInProgress { set; get; }
     }
 }
---
+++ dosymep.Revit.ServerClient/DataContracts/ObjectData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace dosymep.Revit.ServerClient.DataContracts {
+    /// <summary>
+    /// The object data.
+    /// </summary>
+    public abstract class ObjectData {
+        /// <summary>
+        /// The name of folder/model.
+        /// </summary>
+        public string Name { set; get; }
+        
+        /// <summary>
+        /// The lock state of the folder/model.
+        /// </summary>
+        public LockState LockState { set; get; }
+        
+        /// <summary>
+        /// The context of the admin lock on the folder/model,
+        /// describing the use of the admin lock such as copying or moving a folder
+        /// from one server to another.
+        /// </summary>
+        public LockContext LockContext { set; get; }
+
+        /// <summary>
+        /// The list of descendant models that are locked by the Revit clients.
+        /// </summary>
+        public List<ModelLockData> ModelLocksInProgress { set; get; }
+    }
+}
