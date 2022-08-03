ac64c82e7d81c4266b273b1938ea084af1002995
dosymep <dosymep@gmail.com>
2022-08-03 16:04:05 +0300

    added server properties test

---
+++ dosymep.Revit.ServerClient.Tests/ServerClientTests.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+
+using dosymep.Revit.ServerClient.DataContracts;
+
+using NUnit.Framework;
+
+namespace dosymep.Revit.ServerClient.Tests {
+    public class ServerClientTests {
+        private IServerClient _serverClient;
+
+        [SetUp]
+        public void Setup() {
+            _serverClient = new ServerClientBuilder()
+                .SetServerName("10.2.0.144")
+                .SetServerVersion("2022")
+                .Build();
+        }
+
+        [Test]
+        public async Task ServerPropertiesTest() {
+            ServerProperties serverProperties = await _serverClient.GetServerPropertiesAsync();
+            
+            Assert.AreEqual(serverProperties.MachineName, "REVIT-TEST");
+            Assert.AreEqual(serverProperties.MaximumModelNameLength, 40);
+            Assert.AreEqual(serverProperties.MaximumFolderPathLength, 98);
+
+            Assert.AreEqual(serverProperties.Servers, new[] {"10.2.0.144"});
+            Assert.AreEqual(serverProperties.ServerRoles,
+                new[] {ServerRole.Host, ServerRole.Accelerator, ServerRole.Admin});
+
+            Assert.AreEqual(serverProperties.AccessLevelTypes, null);
+        }
+    }
+}
