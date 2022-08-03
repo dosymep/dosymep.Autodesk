8a1835d52ab45c487b1543790aacefe2304974eb
dosymep <dosymep@gmail.com>
2022-08-03 15:31:34 +0300

    added unit tests project

---
+++ dosymep.Revit.ServerClient.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace dosymep.Revit.ServerClient.Tests {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
