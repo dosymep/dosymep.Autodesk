b4d735b670e105d710ead53134a104351835653a
dosymep <dosymep@gmail.com>
2022-08-05 13:13:35 +0300

    added trasformer interface

---
+++ dosymep.Revit.Journaling/ITransformer.cs
@@ -0,0 +1,21 @@
+﻿namespace dosymep.Revit.Journaling {
+    /// <summary>
+    /// Marker transform interface.
+    /// </summary>
+    public interface ITransformer {
+    }
+
+    /// <summary>
+    /// Transform interface.
+    /// </summary>
+    /// <typeparam name="T">Transform type result.</typeparam>
+    /// <typeparam name="TVisitable">Visitable type element.</typeparam>
+    public interface ITransformer<T, in TVisitable> {
+        /// <summary>
+        /// Transform method.
+        /// </summary>
+        /// <param name="visitable">Visitable element.</param>
+        /// <returns>Returns transform result.</returns>
+        T Transform(TVisitable visitable);
+    }
+}
