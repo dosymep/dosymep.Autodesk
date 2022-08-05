636850f4a59056c6fa2bc186cd0d87619718b16f
dosymep <dosymep@gmail.com>
2022-08-05 13:14:05 +0300

    added journal elements

---
+++ dosymep.Revit.Journaling/JournalElements/SyncCentralModelElement.cs
@@ -0,0 +1,47 @@
+﻿namespace dosymep.Revit.Journaling.JournalElements {
+    /// <summary>
+    /// Synchronization central model journal element.
+    /// </summary>
+    public class SyncCentralModelElement : JournalElement {
+        /// <summary>
+        /// Creates synchronization central model journal element.
+        /// </summary>
+        public SyncCentralModelElement() 
+            : base("Synchronization shared model.") {
+        }
+        
+        /// <summary>
+        /// If true compact central model when sync.
+        /// </summary>
+        public bool Compact { get; set; } = true;
+        
+        /// <summary>
+        /// If true saving local file when sync.
+        /// </summary>
+        public bool SaveLocalFile { get; set; } = false;
+        
+        /// <summary>
+        /// If true release borrowed elements when sync.
+        /// </summary>
+        public bool BorrowedElements { get; set; } = false;
+        
+        /// <summary>
+        /// If true release borrowed worksets when sync.
+        /// </summary>
+        public bool UserCreatedWorksets { get; set; } = false;
+
+        /// <summary>
+        /// Comment text when sync.
+        /// </summary>
+        public string Comment { get; set; } = "Synchronization central model.";
+
+        /// <inheritdoc />
+        public override T Reduce<T, TVisitable>(ITransformer<T, TVisitable> transformer) {
+            if(transformer is ITransformer<T, SyncCentralModelElement> openSharedModelTransform) {
+                return openSharedModelTransform.Transform(this);
+            }
+            
+            return default;
+        }
+    }
+}
