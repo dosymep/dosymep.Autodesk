8c70f862d33dfe5d9b7085a356fefbab94c752c0
dosymep <dosymep@gmail.com>
2022-08-10 11:31:34 +0300

    fixed timespan format deserialize

--- dosymep.Revit.ServerClient/Internal/JsonSerialization.cs
+++ dosymep.Revit.ServerClient/Internal/JsonSerialization.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using System.Xml;
 
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace dosymep.Revit.ServerClient.Internal {
     /// <summary>
@@ -8,6 +12,14 @@ namespace dosymep.Revit.ServerClient.Internal {
     /// this class using NewtonsoftJson
     /// </summary>
     internal class JsonSerialization {
+        private readonly JsonSerializerSettings _jsonSerializerSettings
+            = new JsonSerializerSettings() {
+                TypeNameHandling = TypeNameHandling.Objects,
+                NullValueHandling = NullValueHandling.Ignore,
+                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+                Converters = new List<JsonConverter>() {new IsoDateTimeConverter(), new TimeSpanConverter()}
+            };
+
         public string FileExtension => ".json";
 
         public string Serialize<T>(T @object) {
@@ -15,7 +27,7 @@ namespace dosymep.Revit.ServerClient.Internal {
                 throw new ArgumentNullException(nameof(@object));
             }
 
-            return JsonConvert.SerializeObject(@object);
+            return JsonConvert.SerializeObject(@object, _jsonSerializerSettings);
         }
 
         public T Deserialize<T>(string text) {
@@ -23,7 +35,33 @@ namespace dosymep.Revit.ServerClient.Internal {
                 throw new ArgumentException("Value cannot be null or empty.", nameof(text));
             }
 
-            return JsonConvert.DeserializeObject<T>(text);
+            return JsonConvert.DeserializeObject<T>(text, _jsonSerializerSettings);
+        }
+    }
+
+    /// <summary>
+    /// https://stackoverflow.com/questions/12633588/parsing-iso-duration-with-json-net
+    /// </summary>
+    internal class TimeSpanConverter : JsonConverter {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+            var ts = (TimeSpan) value;
+            var tsString = XmlConvert.ToString(ts);
+            serializer.Serialize(writer, tsString);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer) {
+            if(reader.TokenType == JsonToken.Null) {
+                return null;
+            }
+
+            object defaultValue = objectType == typeof(TimeSpan?) ? (object) null : TimeSpan.Zero;
+            var value = serializer.Deserialize<string>(reader);
+            return string.IsNullOrEmpty(value) ? defaultValue : XmlConvert.ToTimeSpan(value);
+        }
+
+        public override bool CanConvert(Type objectType) {
+            return objectType == typeof(TimeSpan) || objectType == typeof(TimeSpan?);
         }
     }
 }
