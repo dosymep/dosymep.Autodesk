f4334eabd59ab168c5e79334852137895586a1e5
dosymep <dosymep@gmail.com>
2022-08-12 11:40:14 +0300

    added IEquatable

--- dosymep.Revit.ServerClient/DataContracts/ObjectData.cs
+++ dosymep.Revit.ServerClient/DataContracts/ObjectData.cs
@@ -1,20 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace dosymep.Revit.ServerClient.DataContracts {
     /// <summary>
     /// The object data.
     /// </summary>
-    public abstract class ObjectData {
+    public abstract class ObjectData : IEquatable<ObjectData> {
         /// <summary>
         /// The name of folder/model.
         /// </summary>
         public string Name { set; get; }
-        
+
         /// <summary>
         /// The lock state of the folder/model.
         /// </summary>
         public LockState LockState { set; get; }
-        
+
         /// <summary>
         /// The context of the admin lock on the folder/model,
         /// describing the use of the admin lock such as copying or moving a folder
@@ -26,5 +27,57 @@ namespace dosymep.Revit.ServerClient.DataContracts {
         /// The list of descendant models that are locked by the Revit clients.
         /// </summary>
         public List<ModelLockData> ModelLocksInProgress { set; get; }
+
+        /// <inheritdoc />
+        public override string ToString() {
+            return Name;
+        }
+
+        #region IEquatable<ObjectData>
+
+        /// <inheritdoc />
+        public bool Equals(ObjectData other) {
+            if(ReferenceEquals(null, other)) {
+                return false;
+            }
+
+            if(ReferenceEquals(this, other)) {
+                return true;
+            }
+
+            return string.Equals(Name, other.Name, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) {
+            if(ReferenceEquals(null, obj)) {
+                return false;
+            }
+
+            if(ReferenceEquals(this, obj)) {
+                return true;
+            }
+
+            if(obj.GetType() != this.GetType()) {
+                return false;
+            }
+
+            return Equals((ObjectData) obj);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode() {
+            return (Name != null ? StringComparer.CurrentCultureIgnoreCase.GetHashCode(Name) : 0);
+        }
+
+        public static bool operator ==(ObjectData left, ObjectData right) {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ObjectData left, ObjectData right) {
+            return !Equals(left, right);
+        }
+
+        #endregion
     }
 }
--- dosymep.Revit.ServerClient/DataContracts/RelativePathData.cs
+++ dosymep.Revit.ServerClient/DataContracts/RelativePathData.cs
@@ -1,11 +1,65 @@
-﻿namespace dosymep.Revit.ServerClient.DataContracts {
+﻿using System;
+
+namespace dosymep.Revit.ServerClient.DataContracts {
     /// <summary>
     /// The relative path data.
     /// </summary>
-    public class RelativePathData {
+    public class RelativePathData : IEquatable<RelativePathData> {
         /// <summary>
         /// The folder path.
         /// </summary>
         public string Path { set; get; }
+
+        /// <inheritdoc />
+        public override string ToString() {
+            return Path;
+        }
+
+        #region IEquatable<RelativePathData>
+
+        /// <inheritdoc />
+        public bool Equals(RelativePathData other) {
+            if(ReferenceEquals(null, other)) {
+                return false;
+            }
+
+            if(ReferenceEquals(this, other)) {
+                return true;
+            }
+
+            return string.Equals(Path, other.Path, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) {
+            if(ReferenceEquals(null, obj)) {
+                return false;
+            }
+
+            if(ReferenceEquals(this, obj)) {
+                return true;
+            }
+
+            if(obj.GetType() != this.GetType()) {
+                return false;
+            }
+
+            return Equals((RelativePathData) obj);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode() {
+            return (Path != null ? StringComparer.CurrentCultureIgnoreCase.GetHashCode(Path) : 0);
+        }
+
+        public static bool operator ==(RelativePathData left, RelativePathData right) {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(RelativePathData left, RelativePathData right) {
+            return !Equals(left, right);
+        }
+
+        #endregion
     }
 }
