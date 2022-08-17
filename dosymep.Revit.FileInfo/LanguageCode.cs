9929e58e4222fb1f284df5f4300775de4b588a07
dosymep <dosymep@gmail.com>
2022-08-17 18:20:55 +0300

    added hungarian language code

--- dosymep.Revit.FileInfo/LanguageCode.cs
+++ dosymep.Revit.FileInfo/LanguageCode.cs
@@ -75,6 +75,11 @@ namespace dosymep.Revit.FileInfo {
         /// Czech
         /// </summary>
         public static readonly LanguageCode CSY = new LanguageCode("CSY", CultureInfo.GetCultureInfo("cs-CZ"));
+        
+        /// <summary>
+        /// Hungarian
+        /// </summary>
+        public static readonly LanguageCode HUN = new LanguageCode("Hun", CultureInfo.GetCultureInfo("hu-HU"));
 
         /// <summary>
         /// Unknown
@@ -91,34 +96,51 @@ namespace dosymep.Revit.FileInfo {
                 throw new ArgumentException("Value cannot be null or empty.", nameof(languageCode));
             }
 
-            if(languageCode.Equals(ENU.Code) || languageCode.Equals("English_USA")) {
+            if(languageCode.Equals(ENU.Code, StringComparison.CurrentCultureIgnoreCase)
+               || languageCode.Equals("English_USA", StringComparison.CurrentCultureIgnoreCase)) {
                 return ENU;
-            } else if(languageCode.Equals(ENG.Code) || languageCode.Equals("English_GB")) {
+            } else if(languageCode.Equals(ENG.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("English_GB", StringComparison.CurrentCultureIgnoreCase)) {
                 return ENG;
-            } else if(languageCode.Equals(FRA.Code) || languageCode.Equals("French")) {
+            } else if(languageCode.Equals(FRA.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("French", StringComparison.CurrentCultureIgnoreCase)) {
                 return FRA;
-            } else if(languageCode.Equals(DEU.Code) || languageCode.Equals("German")) {
+            } else if(languageCode.Equals(DEU.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("German", StringComparison.CurrentCultureIgnoreCase)) {
                 return DEU;
-            } else if(languageCode.Equals(ITA.Code) || languageCode.Equals("Italian")) {
+            } else if(languageCode.Equals(ITA.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("Italian", StringComparison.CurrentCultureIgnoreCase)) {
                 return ITA;
-            } else if(languageCode.Equals(JPN.Code) || languageCode.Equals("Japanese")) {
+            } else if(languageCode.Equals(JPN.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("Japanese", StringComparison.CurrentCultureIgnoreCase)) {
                 return JPN;
-            } else if(languageCode.Equals(KOR.Code) || languageCode.Equals("Korean")) {
+            } else if(languageCode.Equals(KOR.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("Korean", StringComparison.CurrentCultureIgnoreCase)) {
                 return KOR;
-            } else if(languageCode.Equals(PLK.Code) || languageCode.Equals("Polish")) {
+            } else if(languageCode.Equals(PLK.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("Polish", StringComparison.CurrentCultureIgnoreCase)) {
                 return PLK;
-            } else if(languageCode.Equals(ESP.Code) || languageCode.Equals("Spanish")) {
+            } else if(languageCode.Equals(ESP.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("Spanish", StringComparison.CurrentCultureIgnoreCase)) {
                 return ESP;
-            } else if(languageCode.Equals(CHS.Code) || languageCode.Equals("Chinese_Simplified")) {
+            } else if(languageCode.Equals(CHS.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("Chinese_Simplified", StringComparison.CurrentCultureIgnoreCase)) {
                 return CHS;
-            } else if(languageCode.Equals(CHT.Code) || languageCode.Equals("Chinese_Traditional")) {
+            } else if(languageCode.Equals(CHT.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("Chinese_Traditional", StringComparison.CurrentCultureIgnoreCase)) {
                 return CHT;
-            } else if(languageCode.Equals(PTB.Code) || languageCode.Equals("Brazilian_Portuguese")) {
+            } else if(languageCode.Equals(PTB.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("Brazilian_Portuguese", StringComparison.CurrentCultureIgnoreCase)) {
                 return PTB;
-            } else if(languageCode.Equals(RUS.Code) || languageCode.Equals("Russian")) {
+            } else if(languageCode.Equals(RUS.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("Russian", StringComparison.CurrentCultureIgnoreCase)) {
                 return RUS;
-            } else if(languageCode.Equals(CSY.Code) || languageCode.Equals("Czech")) {
+            } else if(languageCode.Equals(CSY.Code, StringComparison.CurrentCultureIgnoreCase)
+                      || languageCode.Equals("Czech", StringComparison.CurrentCultureIgnoreCase)) {
                 return CSY;
+            }else if(languageCode.Equals(HUN.Code, StringComparison.CurrentCultureIgnoreCase)
+                     || languageCode.Equals("Hungarian", StringComparison.CurrentCultureIgnoreCase)) {
+                return HUN;
             }
 
             throw new NotSupportedException($"The {languageCode} is not supported.");
