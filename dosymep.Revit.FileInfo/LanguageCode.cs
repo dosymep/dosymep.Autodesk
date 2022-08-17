dc5c975a118b259768ba2e3c0a1697a1c4efcb47
dosymep <dosymep@gmail.com>
2022-08-17 18:47:12 +0300

    added full code and get by culture info

--- dosymep.Revit.FileInfo/LanguageCode.cs
+++ dosymep.Revit.FileInfo/LanguageCode.cs
@@ -1,158 +1,151 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Reflection;
 
 namespace dosymep.Revit.FileInfo {
     /// <summary>
-    /// Revit language code.
+    /// Autodesk language code.
     /// </summary>
     public class LanguageCode {
         /// <summary>
         /// English - United States
         /// </summary>
-        public static readonly LanguageCode ENU = new LanguageCode("ENU", CultureInfo.GetCultureInfo("en-US"));
+        public static readonly LanguageCode ENU
+            = new LanguageCode("ENU", "English_USA", CultureInfo.GetCultureInfo("en-US"));
 
         /// <summary>
         /// English - United Kingdom
         /// </summary>
-        public static readonly LanguageCode ENG = new LanguageCode("ENG", CultureInfo.GetCultureInfo("en-GB"));
+        public static readonly LanguageCode ENG
+            = new LanguageCode("ENG", "English_GB", CultureInfo.GetCultureInfo("en-GB"));
 
         /// <summary>
         /// French
         /// </summary>
-        public static readonly LanguageCode FRA = new LanguageCode("FRA", CultureInfo.GetCultureInfo("fr-FR"));
+        public static readonly LanguageCode FRA
+            = new LanguageCode("FRA", "French", CultureInfo.GetCultureInfo("fr-FR"));
 
         /// <summary>
         /// German
         /// </summary>
-        public static readonly LanguageCode DEU = new LanguageCode("DEU", CultureInfo.GetCultureInfo("de-DE"));
+        public static readonly LanguageCode DEU
+            = new LanguageCode("DEU", "German", CultureInfo.GetCultureInfo("de-DE"));
 
         /// <summary>
         /// Italian
         /// </summary>
-        public static readonly LanguageCode ITA = new LanguageCode("ITA", CultureInfo.GetCultureInfo("it-IT"));
+        public static readonly LanguageCode ITA
+            = new LanguageCode("ITA", "Italian", CultureInfo.GetCultureInfo("it-IT"));
 
         /// <summary>
         /// Japanese
         /// </summary>
-        public static readonly LanguageCode JPN = new LanguageCode("JPN", CultureInfo.GetCultureInfo("ja-JP"));
+        public static readonly LanguageCode JPN
+            = new LanguageCode("JPN", "Japanese", CultureInfo.GetCultureInfo("ja-JP"));
 
         /// <summary>
         /// Korean
         /// </summary>
-        public static readonly LanguageCode KOR = new LanguageCode("KOR", CultureInfo.GetCultureInfo("ko-KR"));
+        public static readonly LanguageCode KOR
+            = new LanguageCode("KOR", "Korean", CultureInfo.GetCultureInfo("ko-KR"));
 
         /// <summary>
         /// Polish
         /// </summary>
-        public static readonly LanguageCode PLK = new LanguageCode("PLK", CultureInfo.GetCultureInfo("pl-PL"));
+        public static readonly LanguageCode PLK
+            = new LanguageCode("PLK", "Polish", CultureInfo.GetCultureInfo("pl-PL"));
 
         /// <summary>
         /// Spanish
         /// </summary>
-        public static readonly LanguageCode ESP = new LanguageCode("ESP", CultureInfo.GetCultureInfo("es"));
+        public static readonly LanguageCode ESP
+            = new LanguageCode("ESP", "Spanish", CultureInfo.GetCultureInfo("es"));
 
         /// <summary>
         /// Simplified Chinese
         /// </summary>
-        public static readonly LanguageCode CHS = new LanguageCode("CHS", CultureInfo.GetCultureInfo("zh-CN"));
+        public static readonly LanguageCode CHS
+            = new LanguageCode("CHS", "Chinese_Simplified", CultureInfo.GetCultureInfo("zh-CN"));
 
         /// <summary>
         /// Traditional Chinese
         /// </summary>
-        public static readonly LanguageCode CHT = new LanguageCode("CHT", CultureInfo.GetCultureInfo("zh-Hant"));
+        public static readonly LanguageCode CHT
+            = new LanguageCode("CHT", "Chinese_Traditional", CultureInfo.GetCultureInfo("zh-Hant"));
 
         /// <summary>
         /// Brazilian Portuguese
         /// </summary>
-        public static readonly LanguageCode PTB = new LanguageCode("PTB", CultureInfo.GetCultureInfo("pt-BR"));
+        public static readonly LanguageCode PTB
+            = new LanguageCode("PTB", "Brazilian_Portuguese", CultureInfo.GetCultureInfo("pt-BR"));
 
         /// <summary>
         /// Russian
         /// </summary>
-        public static readonly LanguageCode RUS = new LanguageCode("RUS", CultureInfo.GetCultureInfo("ru-RU"));
+        public static readonly LanguageCode RUS
+            = new LanguageCode("RUS", "Russian", CultureInfo.GetCultureInfo("ru-RU"));
 
         /// <summary>
         /// Czech
         /// </summary>
-        public static readonly LanguageCode CSY = new LanguageCode("CSY", CultureInfo.GetCultureInfo("cs-CZ"));
-        
+        public static readonly LanguageCode CSY
+            = new LanguageCode("CSY", "Czech", CultureInfo.GetCultureInfo("cs-CZ"));
+
         /// <summary>
         /// Hungarian
         /// </summary>
-        public static readonly LanguageCode HUN = new LanguageCode("Hun", CultureInfo.GetCultureInfo("hu-HU"));
+        public static readonly LanguageCode HUN
+            = new LanguageCode("HUN", "Hungarian", CultureInfo.GetCultureInfo("hu-HU"));
 
         /// <summary>
         /// Unknown
         /// </summary>
-        public static readonly LanguageCode Unknown = new LanguageCode("Unknown", CultureInfo.CurrentCulture);
+        public static readonly LanguageCode Unknown
+            = new LanguageCode("Unknown", "Unknown", CultureInfo.CurrentCulture);
 
         /// <summary>
-        /// Returns language code by name.
+        /// Returns language code by code or full code.
         /// </summary>
         /// <param name="languageCode">Language code.</param>
-        /// <returns>Returns language code by name.</returns>
+        /// <returns>Returns language code by code or full code.</returns>
         public static LanguageCode GetLanguageCode(string languageCode) {
             if(string.IsNullOrEmpty(languageCode)) {
                 throw new ArgumentException("Value cannot be null or empty.", nameof(languageCode));
             }
 
-            if(languageCode.Equals(ENU.Code, StringComparison.CurrentCultureIgnoreCase)
-               || languageCode.Equals("English_USA", StringComparison.CurrentCultureIgnoreCase)) {
-                return ENU;
-            } else if(languageCode.Equals(ENG.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("English_GB", StringComparison.CurrentCultureIgnoreCase)) {
-                return ENG;
-            } else if(languageCode.Equals(FRA.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("French", StringComparison.CurrentCultureIgnoreCase)) {
-                return FRA;
-            } else if(languageCode.Equals(DEU.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("German", StringComparison.CurrentCultureIgnoreCase)) {
-                return DEU;
-            } else if(languageCode.Equals(ITA.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("Italian", StringComparison.CurrentCultureIgnoreCase)) {
-                return ITA;
-            } else if(languageCode.Equals(JPN.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("Japanese", StringComparison.CurrentCultureIgnoreCase)) {
-                return JPN;
-            } else if(languageCode.Equals(KOR.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("Korean", StringComparison.CurrentCultureIgnoreCase)) {
-                return KOR;
-            } else if(languageCode.Equals(PLK.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("Polish", StringComparison.CurrentCultureIgnoreCase)) {
-                return PLK;
-            } else if(languageCode.Equals(ESP.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("Spanish", StringComparison.CurrentCultureIgnoreCase)) {
-                return ESP;
-            } else if(languageCode.Equals(CHS.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("Chinese_Simplified", StringComparison.CurrentCultureIgnoreCase)) {
-                return CHS;
-            } else if(languageCode.Equals(CHT.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("Chinese_Traditional", StringComparison.CurrentCultureIgnoreCase)) {
-                return CHT;
-            } else if(languageCode.Equals(PTB.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("Brazilian_Portuguese", StringComparison.CurrentCultureIgnoreCase)) {
-                return PTB;
-            } else if(languageCode.Equals(RUS.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("Russian", StringComparison.CurrentCultureIgnoreCase)) {
-                return RUS;
-            } else if(languageCode.Equals(CSY.Code, StringComparison.CurrentCultureIgnoreCase)
-                      || languageCode.Equals("Czech", StringComparison.CurrentCultureIgnoreCase)) {
-                return CSY;
-            }else if(languageCode.Equals(HUN.Code, StringComparison.CurrentCultureIgnoreCase)
-                     || languageCode.Equals("Hungarian", StringComparison.CurrentCultureIgnoreCase)) {
-                return HUN;
+            return GetLanguageCodes()
+                       .FirstOrDefault(item =>
+                           languageCode.Equals(item.Code, StringComparison.CurrentCultureIgnoreCase)
+                           || languageCode.Equals(item.FullCode, StringComparison.CurrentCultureIgnoreCase))
+                   ?? throw new NotSupportedException($"The {languageCode} is not supported.");
+        }
+
+        /// <summary>
+        /// Returns language code by culture info.
+        /// </summary>
+        /// <param name="cultureInfo">Culture info.</param>
+        /// <returns>Returns language code by culture info.</returns>
+        public static LanguageCode GetLanguageCode(CultureInfo cultureInfo) {
+            if(cultureInfo == null) {
+                throw new ArgumentNullException(nameof(cultureInfo));
             }
 
-            throw new NotSupportedException($"The {languageCode} is not supported.");
+            return GetLanguageCodes()
+                       .FirstOrDefault(item => item.CultureInfo.Equals(cultureInfo))
+                   ?? throw new NotSupportedException($"The {cultureInfo} is not supported.");
         }
 
         /// <summary>
         /// Creates language code object. 
         /// </summary>
         /// <param name="code">Revit language code value.</param>
+        /// <param name="fullCode">Revit language full code value.</param>
         /// <param name="cultureInfo">Culture info.</param>
-        private LanguageCode(string code, CultureInfo cultureInfo) {
+        private LanguageCode(string code, string fullCode, CultureInfo cultureInfo) {
             Code = code;
+            FullCode = fullCode;
             CultureInfo = cultureInfo;
         }
 
@@ -160,6 +153,11 @@ namespace dosymep.Revit.FileInfo {
         /// Language code value.
         /// </summary>
         public string Code { get; }
+
+        /// <summary>
+        /// Language full code value.
+        /// </summary>
+        public string FullCode { get; }
 
         /// <summary>
         /// Current culture info.
@@ -172,8 +170,19 @@ namespace dosymep.Revit.FileInfo {
         public string DisplayName => CultureInfo?.DisplayName;
 
         /// <summary>
-        /// Display name.
+        /// English name.
         /// </summary>
         public string EnglishName => CultureInfo.EnglishName;
+
+        /// <summary>
+        /// Returns all supported language codes.
+        /// </summary>
+        /// <returns>Returns all supported language codes.</returns>
+        protected static IEnumerable<LanguageCode> GetLanguageCodes() {
+            return typeof(LanguageCode)
+                .GetFields(BindingFlags.Static | BindingFlags.Public)
+                .Select(item => item.GetValue(null))
+                .OfType<LanguageCode>();
+        }
     }
 }
