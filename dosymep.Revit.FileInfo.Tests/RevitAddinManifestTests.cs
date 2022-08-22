36e9395ddea7dad05e66f0c1818e38cbde0567e4
dosymep <dosymep@gmail.com>
2022-08-22 16:33:18 +0300

    added revit addin manifest tests

---
+++ dosymep.Revit.FileInfo.Tests/RevitAddinManifestTests.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+using dosymep.Revit.FileInfo.RevitAddins;
+
+using NUnit.Framework;
+
+namespace dosymep.Revit.FileInfo.Tests {
+    public class RevitAddinManifestTests {
+        [SetUp]
+        public void Setup() {
+
+        }
+
+        [TearDown]
+        public void Teardown() {
+
+        }
+
+        [Test]
+        [TestCase(@"TestFiles\ModPlus.addin")]
+        [TestCase(@"TestFiles\pyRevit.addin")]
+        [TestCase(@"TestFiles\RevitLookup.addin")]
+        [TestCase(@"TestFiles\Autodesk.AddInManager.addin")]
+        public void CreateRevitAddinManifestTest(string fullFilePath) {
+            var manifest = RevitAddinManifest.CreateAddinManifest(fullFilePath);
+            Assert.NotNull(manifest);
+        }
+    }
+}
